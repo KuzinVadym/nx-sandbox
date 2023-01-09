f102bab8cb948726c432e40032d76ac4c65e020c
Vadym <vadym@ninetailed.com>
2023-01-09 23:22:13 +0100

    add prisma to project

--- apps/nest-api/src/users/users.module.ts
+++ apps/nest-api/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import {Module} from '@nestjs/common';
 import {UsersService} from './users.service';
 import {UsersResolver} from './users.resolver';
+import {PrismaClientService} from '@nx-sandbox/prisma-client';
 
 @Module({
     imports:[],
-    providers: [UsersService, UsersResolver],
+    providers: [UsersService, UsersResolver, PrismaClientService],
     exports: [UsersService]
 })
 export class UsersModule {}
